6102a2fc8e4ecb7176a1985be963216626565b07
unknown <yvanparong1@gmail.com>
2024-04-26 11:16:33 -0700

    dynamic multiline chart

--- boilerplate/projectsite/projectsite/urls.py
+++ boilerplate/projectsite/projectsite/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country
 from fire import views
 
 urlpatterns = [
+    
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+
 
 ]
